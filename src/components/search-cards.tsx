1f42d9533ecd2e544aba8da12468d5de20d1e084
oghenero <sigbenuoghenero@gmail.com>
2022-03-11 09:56:37 +0100

    updates

--- src/components/search-cards.tsx
+++ src/components/search-cards.tsx
@@ -4,10 +4,16 @@ interface ErrorTypeProps {
     title: string,
     rate: number;
     searchFigureYesterday: number,
-    searchFigureLastFriday: number
+    searchFigureLastFriday: number,
+    dataTitle: string,
+    dataValue: number,
+    secDataTitle: string,
+    secDataValue: number,
+    dataDetail: string,
+    features: any
 }
 
-function SearchCard({icon, rate, searchFigureLastFriday, searchFigureYesterday, title}: ErrorTypeProps) {
+function SearchCard({icon, rate, dataTitle,features, dataDetail, secDataTitle, secDataValue, dataValue, searchFigureLastFriday, searchFigureYesterday, title}: ErrorTypeProps) {
   return (
     <div className="search-section__card">
         <div className="search-section__card__flex">
@@ -16,16 +22,19 @@ function SearchCard({icon, rate, searchFigureLastFriday, searchFigureYesterday, 
         {icon}
         </div>
         <div className="search-section__detail">
-            <h4 className="search-section__title_p">{title} <span className="search-section__icon__rate">{rate}%</span></h4>
+            <h4 className="search-section__title_p">{title} {rate === 0 ? " " :<span className="search-section__icon__rate">{rate}%</span>}</h4>
             <span className="search-section__title flex"><h3>{searchFigureYesterday} </h3><p>Yesterday</p></span>
             <span className="search-section__value flex"><h3>{searchFigureLastFriday} </h3><p>Last friday</p></span>
         </div>
         </div>
         <div className="search-section__traffic">
-            <h4>Mobile traffic: 100%</h4>
-            <h4>Web traffic: 100%</h4>
-            <div className="search-section__value"><p>You get 100% traffic on mobile and desktop devices</p></div>
-            <div>Help: <p>Searches, Presentation</p></div>
+            <h4>{dataTitle}: {dataValue}%</h4>
+            <h4>{secDataTitle}: {secDataValue}%</h4>
+            <div className="search-section__value">
+              <p>{dataDetail}</p>
+              </div>
+            <div>Help: <p>{features}</p></div>
+            {/* <div>Help: <p>Searches, Presentation</p></div> */}
         </div>
     </div>
   );
